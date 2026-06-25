f7a11e9016ffd2689245064614cb9c1c07da61b3
anushagurijala26 <anushasasi11@gmail.com>
2026-06-26 01:45:55 +0530

    Add files via upload
    assignment 1

---
+++ Assignment1.js
@@ -0,0 +1,10 @@
+let firstName ="Anusha"
+console.log(typeof firstName);
+let companyName ="TCS"
+console.log(typeof companyName);
+let mobileNumber =90909090
+console.log(typeof mobileNumber);
+let isAutomation =true
+console.log(typeof isAutomation);
+let hasPlaywright
+console.log(typeof hasPlaywright);
