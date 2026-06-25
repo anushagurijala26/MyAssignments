d5a3646faeb1ced6e191ebf47206da8200b70ab6
anushagurijala26 <anushasasi11@gmail.com>
2026-06-26 02:07:18 +0530

    Add files via upload

---
+++ varletconstAssignment.js
@@ -0,0 +1,13 @@
+function getBrowserName(){
+    const browserName ="Chrome"
+
+    if (browserName==="Chrome")
+    {   
+        let browserName ="local browser name"
+        console.log(browserName);
+
+    }
+        
+    
+}
+getBrowserName()
